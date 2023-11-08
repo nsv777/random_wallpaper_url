c4b06d1d0c5a309b045626a7af5488c6c7a0ea69
Stanislav Nilov <stanislav.nilov@ringcentral.com>
2023-11-08 10:26:35 +0000

    updated soup filter

--- random_wallpaper_url.py
+++ random_wallpaper_url.py
@@ -2,28 +2,40 @@
 import re
 
 import cloudscraper
+import requests
 from bs4 import BeautifulSoup
 
-alphacoders_url = "https://wall.alphacoders.com/"
+alphacoders_url = "https://wall.alphacoders.com"
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
 }
+browser = {
+    'browser': 'firefox',
+    'platform': 'linux',
+    'desktop': True
+}
 
 
-def _get_big_url(url, scraper):
-    soup = BeautifulSoup(scraper.get(url + "random.php", headers=headers).text, features="lxml")
+class Wallpaper(object):
+    def __init__(self):
+        self.session = requests.session()
+        self.scraper = cloudscraper.create_scraper(delay=20, browser=browser, sess=self.session)
 
-    return soup.find("a", {"href": re.compile('big\.php\?i=\d+')})["href"]
+    def _get_big_url(self, url: str):
+        scraper_text = self.scraper.get(f"{url}/random.php", headers=headers).text
+        soup = BeautifulSoup(scraper_text, features="lxml")
 
+        return soup.find("a", {"href": re.compile('big\.php\?i=\d+')})["href"]
 
-def get_wallpaper_url(url):
-    scraper = cloudscraper.create_scraper(delay=10, browser='chrome')
-    soup = BeautifulSoup(scraper.get(url + _get_big_url(url=url, scraper=scraper), headers=headers).text, features="lxml")
+    def get_wallpaper_url(self, url: str) -> str:
+        big_url = self._get_big_url(url=url)
+        soup = BeautifulSoup(self.scraper.get(f"{url}{big_url}", headers=headers).text,
+                             features="lxml")
 
-    return soup.find("div", {"class": "center img-container-desktop"}).find("img")["src"]
+        return soup.find("picture").find("img", {"id": "main-content"})["src"]
 
 
 if __name__ == "__main__":
     # pp = pprint.PrettyPrinter()
-    print(get_wallpaper_url(url=alphacoders_url))
-
+    wallpaper = Wallpaper()
+    print(wallpaper.get_wallpaper_url(url=alphacoders_url))
